1b1a64e18cbced2f8ed0fb3cf813a5fe3cb8d332
Marco Medri <marcun.medri@gmail.com>
2023-04-03 23:16:07 +0200

    Aggiunta icona

--- lib/main.dart
+++ lib/main.dart
@@ -106,66 +106,66 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
 
   Widget buildRightSide({vertical = true}) {
     return Padding(
-      padding: EdgeInsets.all(vertical? 5 : 2),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          const Spacer(),
-          Text(previousCalculation, style: TextStyle(fontSize: vertical? 48 : 36)),
-          const SizedBox(height: 16),
-          Text(currentCalculation, style: TextStyle(fontSize: vertical? 96 : 48)),
-          const SizedBox(height: 16),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              buildButton('C', color: Colors.redAccent, onPressed: clear, textColor: Colors.black, vertical: vertical),
-              buildButton('±', color: Colors.white60, onPressed: changeSign, textColor: Colors.black, vertical: vertical),
-              buildButton('%', color: Colors.white60, onPressed: () => updateCalculation('/100'), textColor: Colors.black, vertical: vertical),
-              buildButton('÷', color: Colors.orange, onPressed: () => updateCalculation('/'), vertical: vertical),
-            ],
-          ),
-          SizedBox(height: vertical? 16 : 4),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              buildButton('7', onPressed: () => updateCalculation('7'), vertical: vertical),
-              buildButton('8', onPressed: () => updateCalculation('8'), vertical: vertical),
-              buildButton('9', onPressed: () => updateCalculation('9'), vertical: vertical),
-              buildButton('×', color: Colors.orange, onPressed: () => updateCalculation('*'), vertical: vertical),
-            ],
-          ),
-          SizedBox(height: vertical? 16 : 4),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              buildButton('4', onPressed: () => updateCalculation('4'), vertical: vertical),
-              buildButton('5', onPressed: () => updateCalculation('5'), vertical: vertical),
-              buildButton('6', onPressed: () => updateCalculation('6'), vertical: vertical),
-              buildButton('-', color: Colors.orange, onPressed: () => updateCalculation('-'), vertical: vertical),
-            ],
-          ),
-          SizedBox(height: vertical? 16 : 4),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              buildButton('1', onPressed: () => updateCalculation('1'), vertical: vertical),
-              buildButton('2', onPressed: () => updateCalculation('2'), vertical: vertical),
-              buildButton('3', onPressed: () => updateCalculation('3'), vertical: vertical),
-              buildButton('+', color: Colors.orange, onPressed: () => updateCalculation('+'), vertical: vertical),
-            ],
-          ),
-          SizedBox(height: vertical? 16 : 4),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              buildButton('0', onPressed: () => updateCalculation('0'), vertical: vertical, zero: true),
-              buildButton(',', onPressed: () => updateCalculation(','), vertical: vertical),
-              buildButton('=', color: Colors.orange, onPressed: evaluate, vertical: vertical),
-            ],
-          ),
-          SizedBox(height: vertical? 16 : 4),
-        ],
-      )
+        padding: EdgeInsets.all(vertical? 5 : 2),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            const Spacer(),
+            Text(previousCalculation, style: TextStyle(fontSize: vertical? 48 : 36)),
+            const SizedBox(height: 16),
+            Text(currentCalculation, style: TextStyle(fontSize: vertical? 96 : 48)),
+            const SizedBox(height: 16),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                buildButton('C', color: Colors.redAccent, onPressed: clear, textColor: Colors.black, vertical: vertical),
+                buildButton('±', color: Colors.white60, onPressed: changeSign, textColor: Colors.black, vertical: vertical),
+                buildButton('%', color: Colors.white60, onPressed: () => updateCalculation('/100'), textColor: Colors.black, vertical: vertical),
+                buildButton('÷', color: Colors.orange, onPressed: () => updateCalculation('/'), vertical: vertical),
+              ],
+            ),
+            SizedBox(height: vertical? 16 : 4),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                buildButton('7', onPressed: () => updateCalculation('7'), vertical: vertical),
+                buildButton('8', onPressed: () => updateCalculation('8'), vertical: vertical),
+                buildButton('9', onPressed: () => updateCalculation('9'), vertical: vertical),
+                buildButton('×', color: Colors.orange, onPressed: () => updateCalculation('*'), vertical: vertical),
+              ],
+            ),
+            SizedBox(height: vertical? 16 : 4),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                buildButton('4', onPressed: () => updateCalculation('4'), vertical: vertical),
+                buildButton('5', onPressed: () => updateCalculation('5'), vertical: vertical),
+                buildButton('6', onPressed: () => updateCalculation('6'), vertical: vertical),
+                buildButton('-', color: Colors.orange, onPressed: () => updateCalculation('-'), vertical: vertical),
+              ],
+            ),
+            SizedBox(height: vertical? 16 : 4),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                buildButton('1', onPressed: () => updateCalculation('1'), vertical: vertical),
+                buildButton('2', onPressed: () => updateCalculation('2'), vertical: vertical),
+                buildButton('3', onPressed: () => updateCalculation('3'), vertical: vertical),
+                buildButton('+', color: Colors.orange, onPressed: () => updateCalculation('+'), vertical: vertical),
+              ],
+            ),
+            SizedBox(height: vertical? 16 : 4),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                buildButton('0', onPressed: () => updateCalculation('0'), vertical: vertical, zero: true),
+                buildButton(',', onPressed: () => updateCalculation(','), vertical: vertical),
+                buildButton('=', color: Colors.orange, onPressed: evaluate, vertical: vertical),
+              ],
+            ),
+            SizedBox(height: vertical? 16 : 4),
+          ],
+        )
     );
   }
 
@@ -177,36 +177,25 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
     return buildRightSide(vertical: false);
   }
 
-  Widget buildButton(String text, {zero= false, vertical = true, Color? color = Colors.black38, void Function()? onPressed, Color? textColor = Colors.white}) {/*
+  Widget buildButton(String text, {zero= false, vertical = true, Color? color = Colors.black38, void Function()? onPressed, Color? textColor = Colors.white}) {
     return ConstrainedBox(
-      constraints: const BoxConstraints(
-        minWidth: 48,
-        minHeight: 48,
-        maxWidth: double.infinity,
-        maxHeight: double.infinity,
-      ),
-      child: ElevatedButton(
-        style: ElevatedButton.styleFrom(
-          primary: color ?? Theme.of(context).colorScheme.primary,
-          onPrimary: Theme.of(context).colorScheme.onPrimary,
-          shape: CircleBorder(),
-          padding: EdgeInsets.all(24),
+        constraints: const BoxConstraints(
+          minWidth: 48,
+          minHeight: 48,
+          maxWidth: 60,
+          maxHeight: 60,
         ),
-        onPressed: onPressed,
-        child: Text(text, style: TextStyle(fontSize: 24,color: textColor)),
-      ),
-    );*/
-
-    return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        foregroundColor: Theme.of(context).colorScheme.onPrimary, backgroundColor: color ?? Theme.of(context).colorScheme.primary,
-        shape: zero? RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(45),
-        ) : const CircleBorder(),
-        minimumSize: vertical? zero? const Size(352, 128) : const Size(128, 128) : zero? const Size(150, 64) : const Size (64, 64),
-      ),
-      onPressed: onPressed,
-      child: Text(text, style: TextStyle(fontSize: vertical? 36 : 18,color: textColor)),
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            foregroundColor: Theme.of(context).colorScheme.onPrimary, backgroundColor: color ?? Theme.of(context).colorScheme.primary,
+            shape: zero? RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(45),
+            ) : const CircleBorder(),
+            minimumSize: vertical? zero? const Size(352, 128) : const Size(128, 128) : zero? const Size(150, 64) : const Size (64, 64),
+          ),
+          onPressed: onPressed,
+          child: Text(text, style: TextStyle(fontSize: vertical? 36 : 18,color: textColor)),
+        )
     );
   }
 
